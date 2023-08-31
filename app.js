d172be57b443952b3400ef8c4b640de1ef3cc799
Sameer <ks4031180@gmail.com>
2023-08-31 08:47:36 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var percentage = prompt("Write your percentage")
+
+if(percentage >= 80){
+    alert("A1 grade")
+}else if(percentage < 80 && percentage >= 70){
+    alertA(" grade")
+}else if(percentage < 70 && percentage >= 60){
+    alert("B grade")
+}else if(percentage < 60  && percentage >= 50){
+    alert("C grade")
+}else if(percentage < 50 && percentage >= 40){
+    alert("D grade")
+}else if(percentage < 40 && percentage >= 30){
+    alert("E grade")
+}else{
+    alert("F grade")
+}
+
